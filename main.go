31d1b7774a712662e533ecc94ebed9437513d425
stanleylia <125443888+stanleylia@users.noreply.github.com>
2023-11-14 10:11:44 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,3 +1,8 @@
+/*
+ * Version: 1.0
+ * License: MIT License
+ * License Description: This is a sample license file. You can find the full license terms in LICENSE.txt.
+ */
 package main
 
 import "fmt"
